9add258eba4ac0a52f527448aa1d6206d98fa6d4
sofialoren1 <127851693+sofialoren1@users.noreply.github.com>
2024-06-28 09:02:08 -0300

    Add files via upload

---
+++ aula04/fetch parte4/script.js
@@ -0,0 +1,37 @@
+//Criando a função Fetch do botão pesquisar
+
+async function fetchPokemon(){
+    try{
+        const pokemonId = document.getElementById('pokemonID').value
+        if(pokemonId == ''){
+                alert('Digite um ID válido');
+                throw new Error('ID inválido');
+        }
+            const pokemonNome = document.getElementById('pokemonNome');
+            const pokemonType = document.getElementById('pokemonType');
+
+            const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+                if(!resp.ok){
+                    throw new Error('pokemon não encontrado');
+                } else {
+                    const data = await resp.json();
+                    console.log(data);
+                    const pokemonSprite = data.sprites.front_default;
+                    const imgElemento = document.getElementById('pokemonSprite');
+
+                    const pokemonNomeData = data.name.toUpperCase();
+                    const pokemonTypeData = data.types[0].type.name;
+
+                    //setando os elementos data no HTML
+                    imgElemento.src = pokemonSprite;
+                    imgElemento.style.display = 'block';
+
+                    pokemonNome.innerHTML = pokemonNomeData;
+                    pokemonType.innerHTML = pokemonTypeData;
+                }
+
+    }catch (error){
+        console.log(error);
+    }
+}
+    
