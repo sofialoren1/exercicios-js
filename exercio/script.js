bb4c585f20a095fc329f91ba2801380a99aefa97
sofialoren1 <127851693+sofialoren1@users.noreply.github.com>
2024-07-01 08:38:13 -0300

    Add files via upload

---
+++ exercio/script.js
@@ -0,0 +1,47 @@
+//Selecionando a div pokeContainer
+const pokeContainer =  document.getElementById('pokeContainer');
+const pokemonCount = 100;
+
+//Função que percorre o objeto do Fetch
+const fetchPokemon = async() =>{
+    for(let i=1; i < pokemonCount; i++){
+        await getPokemons(i);
+    }
+}
+
+
+//Buscando ps pokemon pelo Fetch
+const getPokemons  = async(id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.name);
+    createPokemonCard(data);
+}
+
+    //Criando os cards dinâmicamente
+    const createPokemonCard = (poke) =>{
+    const card = document.createElement('div');
+    card.classList.add('pokemon');
+
+    //Inserindo os elementos do card
+    const name = poke.name[0] + poke.name.slice(1);
+    const id = poke.id.toString();
+    const type = poke.types[0].type.name;
+
+    //Criando o card
+    const pokemonInnerHtml = `
+    <div class="imagemContainer">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png" alt="">
+            </div>
+            <div class="info">
+                <span class="number">#${id}</span>
+                <h3 class="name">${name}Bulbasaur</h3>
+                <small class="type">Tipo:<span>${type}Grass</span></small>
+            </div>
+    `
+    card.innerHTML = pokemonInnerHtml;
+    pokeContainer.appendChild(card);
+}
+
+fetchPokemon();
